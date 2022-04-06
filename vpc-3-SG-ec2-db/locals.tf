4b92061956d99f0a0e2b340cb08841bf4d960d95
Adithya119 <48482554+Adithya119@users.noreply.github.com>
2022-04-06 22:01:08 +0530

    Add files via upload

---
+++ vpc-3-SG-ec2-db/locals.tf
@@ -0,0 +1,14 @@
+locals {
+    anywhere   = "0.0.0.0/0"
+    ssh_port   = 22
+    http_port  = 80
+    https_port = 443
+    pg_port    = 5432
+    tcp        = "TCP"
+}
+
+
+# Input variables are like function arguments. 
+# Output values are like function return values.
+# Local values are like a function's temporary local variables.
+# Note:- For brevity, local values are often referred to as just "locals" when the meaning is clear from context.
